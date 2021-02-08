bf931083c4b8b2bd1bda13a5b57eec5025c69712
g94lina <28238824+lina994@users.noreply.github.com>
2021-02-08 08:41:05 +0200

    Add CSS to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,11 +4,11 @@ import s from './Navbar.module.css';
 function Navbar(props) {
   return (
     <nav className={s.appNavbar}>
-      <div>Profile</div>
-      <div>Messages</div>
-      <div>News</div>
-      <div>Music</div>
-      <div>Settings</div>
+      <div className={s.navLinkWrapper}>Profile</div>
+      <div className={s.navLinkWrapper}>Messages</div>
+      <div className={s.navLinkWrapper}>News</div>
+      <div className={s.navLinkWrapper}>Music</div>
+      <div className={s.navLinkWrapper}>Settings</div>
     </nav>
   );
 }
